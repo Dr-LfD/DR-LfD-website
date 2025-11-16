66b93849ee7b85aaeaeb7a2e2078f2186ed56330
anonymous <12345+anon@users.noreply.github.com>
2025-11-16 21:06:39 +0800

    set base path for page deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/DR-LfD-website/' : './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
